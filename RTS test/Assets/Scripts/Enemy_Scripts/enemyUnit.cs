4adefa23e27a86a3d21284f175063f22ec977be9
Clos2024 <56045580+Clos2024@users.noreply.github.com>
2021-05-01 01:13:41 -0500

    Added enemy chase and attack behavior

--- RTS test/Assets/Scripts/Enemy_Scripts/enemyUnit.cs
+++ RTS test/Assets/Scripts/Enemy_Scripts/enemyUnit.cs
@@ -7,7 +7,30 @@ public class enemyUnit : MonoBehaviour
 {
     private Vector3 homePosition,roamPosition;
     private NavMeshAgent agent;
+    [SerializeField]
     private NavMeshPath path;
+    private UnitSelections playerUnits;
+    [SerializeField]
+    private GameObject target;
+
+    public float enemyDetectionRadius;
+    public float attackRange;
+    public float speed;
+    private State state;
+
+    private enum State
+    {
+        Wandering,
+        Chase,
+        Attack,
+    }
+
+    void Awake()
+    {
+        state = State.Wandering;
+        playerUnits = UnitSelections.Instance;
+    }
+
     private void Start()
     {
         agent = transform.GetComponent<NavMeshAgent>();
@@ -15,27 +38,83 @@ public class enemyUnit : MonoBehaviour
         roamPosition = GetRoamingPostion();
         path = new NavMeshPath();
         agent.CalculatePath(new Vector3(100,100,100),path);
+        agent.speed = speed;
     }
 
     private void Update()
     {
-        ///OMFG THIS WORKS IN RESETTING THE PATH SAVE THIS WORK
-        if(path.status == NavMeshPathStatus.PathInvalid || path.status == NavMeshPathStatus.PathPartial)
+        if (target == null && playerUnits.unitList.Count != 0)
         {
-            Debug.Log("reset path");
+            foreach (var unit in playerUnits.unitList)
+            {
+                if (Vector3.Distance(transform.position, unit.transform.position) < enemyDetectionRadius)
+                {
+                    target = unit.transform.gameObject;
+                }
+            }
+        }
+
+        Debug.Log(state);
+        switch(state)
+        {
+            default:
+            case State.Wandering:
+                {
+                    Wander();
+                    if(target != null)
+                    {
+                        state = State.Chase;
+                    }
+                }
+                break;
+            case State.Chase:
+                {
+                    Chase();
+                    if(target == null)
+                    {
+                        state = State.Wandering;
+                    }
+
+                    if(Vector3.Distance(transform.position,target.transform.position) < attackRange)
+                    {
+                        state = State.Attack;
+                    }
+                }
+                break;
+            case State.Attack:
+                {
+                    Attack();
+
+                    if (target == null)
+                    {
+                        state = State.Wandering;
+                        roamPosition = GetRoamingPostion();
+                        agent.CalculatePath(roamPosition, path);
+                        agent.SetPath(path);
+                    }
+                }
+                break;
+        }
+    }
+
+    private void Wander()
+    {
+        if (path.status == NavMeshPathStatus.PathInvalid || path.status == NavMeshPathStatus.PathPartial || Vector3.Distance(transform.position, roamPosition) <= 1f)
+        {
+            Debug.Log(" Wander called");
             roamPosition = GetRoamingPostion();
             agent.CalculatePath(roamPosition, path);
             agent.SetPath(path);
         }
-
-        if(Vector3.Distance(transform.position,roamPosition) < 1f)
-        {
-            Debug.Log("Arrived at path time for new path");
-            roamPosition = GetRoamingPostion();
-            agent.CalculatePath(roamPosition, path);
-            agent.SetPath(path);
-        }
-
+    }
+    private void Chase()
+    {
+        agent.CalculatePath(target.transform.position, path);
+        agent.SetPath(path);
+    }
+    private void Attack()
+    {
+        Destroy(target);
     }
     private Vector3 GetRoamingPostion()
     {
